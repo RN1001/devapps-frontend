a4c5fed52c6e29e9095526ab2f8dd217c14bc585
mnazir <monazir1001@gmail.com>
2020-02-02 20:37:05 +0000

    added test.vue file

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test formName="register"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="test">
+        <h2> {{ formName }} </h2>
+        <form>
+            <p>
+            <label for="username"> Username </label>
+            <input v-model="username" id="username">
+            </p>
+            <p>
+            <label for="password"> Password </label>
+            <input v-model="password" id="password">
+            </p>
+            <p>
+            <label for="age"> Age </label>
+            <input v-model="age" id="age">
+            </p>
+        </form>
+        <section>
+            <h4>You entered: </h4>
+            <p> Username: {{username}} </p>
+            <p> Password: {{password}} </p>
+            <p> Age: {{age}} </p>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data: function() {
+        return {
+            username: "",
+            password: "",
+            age: "" 
+        };
+    },
+    props: {
+        formName: String
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        border: 1px solid black
+    }
+
+    form > #id {
+        border-radius: 5px;
+    }
+</style>
