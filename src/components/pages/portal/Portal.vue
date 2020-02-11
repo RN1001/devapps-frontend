6ff0dfa5678997b719deea404aacdfab4839edd7
mnazir <monazir1001@gmail.com>
2020-02-11 23:30:05 +0000

    Added Portal component to application.

---
+++ src/components/pages/portal/Portal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Programming</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Databases</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">IDE's</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SDK's</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-9">
+                <!-- loads apps depending on link clicked -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Portal"
+    }
+</script>
+
+<style scoped>
+
+</style>
