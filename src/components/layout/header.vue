79711e939ad84317a1e9c3dacae1d21e0ef65bde
mnazir <monazir1001@gmail.com>
2020-02-11 23:30:48 +0000

    Added Portal route to header component.

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="container">
+        <ul class="nav">
+            <li class="nav-link">
+                <router-link class="nav-item" to="/"> Home </router-link>
+            </li>
+            <li class="nav-link">
+                <router-link class="nav-item" to="/portal"> Portal </router-link>
+            </li>
+            <li class="nav-link">
+                <router-link class="nav-item" to="/register"> Register </router-link>
+            </li>
+        </ul>
         <div class="jumbotron">
             <h1> Welcome to DevApps </h1>
         </div>
