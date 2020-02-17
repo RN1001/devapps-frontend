7f105de2c890c97cbcbbf93828430702ea8c24b7
mnazir <monazir1001@gmail.com>
2020-02-17 23:05:47 +0000

    created table with backend api call, added some if/else directives for data that isnt there.

--- src/components/pages/portal/Portal.vue
+++ src/components/pages/portal/Portal.vue
@@ -1,24 +1,40 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-sm-3">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Programming</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Databases</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">IDE's</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">SDK's</a>
-                    </li>
-                </ul>
+            <div class="col-sm-12">
+                <h4> Programming runtime's </h4>
             </div>
-            <div class="col-sm-9">
-                <!-- loads apps depending on link clicked -->
+            <div class="col-sm-12">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th></th>
+                            <th> name </th>
+                            <th> version </th>
+                            <th> description </th>
+                            <th> operating systems </th>
+                        </tr>
+                    </thead>
+                    <tbody v-for="(application, index) in apps" :key="index">
+                        <tr>
+                            <div v-if="application.url != null">
+                                <td> <button class="btn btn-primary" v-on:click="(toUrl(application.url))">open</button> </td>
+                            </div>
+                            <div v-else>
+                                <td> Not available</td>
+                            </div>
+                            <td> {{application['name']}}</td>
+                            <td> {{application['version']}}</td>
+                            <td> {{application['description']}}</td>
+                            <div v-if="application['OS'].length > 0">
+                                <td> {{application['OS']}} </td>
+                            </div>
+                            <div v-else>
+                                <td> no OS </td>
+                            </div>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -26,7 +42,27 @@
 
 <script>
     export default {
-        name: "Portal"
+        name: "Portal",
+        data: function() {
+            return {
+                url: "",
+                apps: this.getData(),
+            }
+        },
+
+        methods: {
+            getData: function() {
+                fetch('http://localhost:3000/applications')
+                    .then((res) => {
+                        return res.json();
+                    }).then((json) => {
+                    this.apps = json;
+                })
+            },
+            toUrl: function(url) {
+                window.open(url);
+            }
+        },
     }
 </script>
 
