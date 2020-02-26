9d3858f77a3e18ad8ff3d4be946e83ec54e36f45
mnazir <monazir1001@gmail.com>
2020-02-26 23:56:39 +0000

    added service to check if user is logged in on route registration. added event to check whether a user is logged in or not.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import Index from './components/Index.vue';
-import Register from './components/pages/register/Register.vue';
+import Register from './components/pages/authentication/Register.vue';
 import Portal from "./components/pages/portal/Portal";
-
+import Login from "./components/pages/authentication/Login";
+import {isLoggedIn} from "./service/authService";
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
+
 
 const router = new VueRouter({
   routes: [
@@ -16,19 +18,45 @@ const router = new VueRouter({
       component: Index,
       props: {
         msg: "Hello"
+      },
+    },
+    {
+      path: "/portal",
+      component: Portal,
+      beforeEnter: (to, from, next) => {
+        if (isLoggedIn) {
+          next();
+        } else {
+          next('/login');
+        }
       }
+    },
+    {
+      path: "/login",
+      component: Login,
+      beforeEnter: (to, from, next) => {
+        if (isLoggedIn) {
+          next();
+        } else {
+          next('/login');
+        }
+      }
+
     },
     {
       path: "/register",
       component: Register,
       props: {
         formName: "Register"
+      },
+      beforeEnter: (to, from, next) => {
+        if (isLoggedIn) {
+          next();
+        } else {
+          next('/login');
+        }
       }
     },
-    {
-      path: "/portal",
-      component: Portal
-    }
   ]
 });
 
