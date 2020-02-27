96eeb712e04fff798a2ccf4ed03baed386ff84b0
mnazir <monazir1001@gmail.com>
2020-02-27 00:02:46 +0000

    view for login page (not finished)

---
+++ src/components/pages/authentication/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <form>
+                    <div class="form-group row">
+                        <label for="username"> Username </label>
+                        <input type="text" class="form-control" id="username" required/>
+                    </div>
+                    <div class="form-group row">
+                        <label for="password"> Password </label>
+                        <input type="password" class="form-control" id="password" required/>
+                    </div>
+                    <div class="form-group row">
+                        <button type="button" v-on:click="userLogin()" class="btn btn-secondary"> Login </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "login",
+        data: function() {
+            return {
+                username: "",
+                password: ""
+            }
+        },
+        methods: {
+            userLogin: function() {
+                axios.post('http://localhost:3000/login', {
+
+                });
+            }
+        }
+    }
+</script>
