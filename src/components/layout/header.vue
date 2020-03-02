6b021936a619e73b23a66a118e3e2dcb8365557c
mnazir <monazir1001@gmail.com>
2020-03-02 01:13:35 +0000

    added if else cluases if a user is logged in or not, when logging out, have to reload the page to get rid of the localstorage token data.

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -7,11 +7,23 @@
             <li class="nav-link">
                 <router-link class="nav-item" to="/portal"> Portal </router-link>
             </li>
-            <li class="nav-link">
+            <li v-if="userLoggedIn() === false" class="nav-link">
                 <router-link class="nav-item" to="/login"> Login </router-link>
             </li>
-            <li class="nav-link">
+            <li v-else>
+            </li>
+            <li v-if="userLoggedIn() === false" class="nav-link">
                 <router-link class="nav-item" to="/register"> Register </router-link>
+            </li>
+            <li v-else>
+            </li>
+            <li v-if="username() !== null" class="nav-link">
+                <router-link class="nav-item" to="/"> {{username()}}'s Account </router-link>
+            </li>
+            <li v-else>
+            </li>
+            <li v-if="userLoggedIn() === true">
+                <a href="#" class="nav-link" v-on:click="logout"> Logout </a>
             </li>
         </ul>
         <div class="jumbotron">
@@ -21,7 +33,22 @@
 </template>
 
 <script>
+    import {logout, isLoggedIn, getUsername} from "../../service/authService";
+
 export default {
-    name: "App-header"
+    name: "App-header",
+    methods: {
+        logout: function() {
+            logout();
+            this.$router.push({name: 'home'});
+            window.location.reload();
+        },
+        userLoggedIn: function () {
+            return isLoggedIn();
+        },
+        username: function () {
+            return getUsername();
+        }
+    }
 }
 </script>
